bb3ade5884c8291e9c40b586c1d3967163cc8d3b
Guilherme Marcondes <guilhermedmlopes@hotmail.com>
2022-11-10 22:26:59 -0300

    All tests and pipeline finished

--- test/validation_test.dart
+++ test/validation_test.dart
@@ -9,10 +9,10 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   print(validarCPF('05351151805'));
   test('validador', () {
-    // expect(validarCPF('05351151805'), false);
+    expect(validarCPF('05351151805'), false);
     expect(validarCPF('05351151804'), true);
     expect(validarCNPJ('08754527000113'), true);
-    // expect(validarCNPJ('08754527000114'), false);
+    expect(validarCNPJ('08754527000114'), false);
   });
 
 }
