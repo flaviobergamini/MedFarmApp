cf2789024c3b166517e84ba01e7a352f8376542c
Flavio Henrique Madureira Bergamini <flaviohenrique@gec.inatel.br>
2022-11-24 00:59:23 -0300

    Criando atributo estático para acesso em diferentes telas

--- lib/Controller/Statics/Utils.dart
+++ lib/Controller/Statics/Utils.dart
@@ -1,9 +1,23 @@
 class Utils {
   static int doctorsId = 0;
+  static int clientIdAppointment = 0;
+  static int appointmentPending = 0;
 
   static void setDoctorId(int doctorId){
     doctorsId = doctorId;
   }
 
+  static void setClientIdAppointment(int clientId){
+    clientIdAppointment = clientId;
+  }
+
+  static void setAppointmentPending(int appointmentId){
+    appointmentPending = appointmentId;
+  }
+
   static int get getDoctorId => doctorsId;
+
+  static int get getClientIdAppointment => clientIdAppointment;
+
+  static int get getAppointmentPending => appointmentPending;
 }
